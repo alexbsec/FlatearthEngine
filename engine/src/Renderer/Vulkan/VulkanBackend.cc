8fbc53da83ac3eb03f0078be826cfedb3ff982e7
alexbsec <j.a.c.buschinelli@gmail.com>
2025-03-03 00:34:52 -0300

    correct comments

--- engine/src/Renderer/Vulkan/VulkanBackend.cc
+++ engine/src/Renderer/Vulkan/VulkanBackend.cc
@@ -280,14 +280,14 @@ void VulkanBackend::DeviceDestroy() {
   _context.device.transferQueue = nullptr;
 
   if (_context.device.logicalDevice) {
-    FINFO("DestroyDevice(): Destroying logical device...");
+    FINFO("VulkanBackend::DeviceDestroy(): Destroying logical device...");
     vkDestroyDevice(_context.device.logicalDevice, _context.allocator);
     _context.device.logicalDevice = nullptr;
-    FINFO("DestroyDevice(): Logical device destroyed");
+    FINFO("VulkanBackend::DeviceDestroy(): Logical device destroyed");
   }
 
   // Releasing resources on physical device
-  FINFO("DestroyDevice(): Releasing physical device resources...");
+  FINFO("VulkanBackend::DeviceDestroy(): Releasing physical device resources...");
   _context.device.physicalDevice = nullptr;
 
   if (_context.device.swapchainSupport.formats) {
