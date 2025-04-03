4d6eb14b82ebd8b978d16af15fe4e6394b82dd99
alexbsec <j.a.c.buschinelli@gmail.com>
2025-04-02 23:11:27 -0300

    linear allocator tests

--- tests/src/Memory/LinearAllocatorTests.cc
+++ tests/src/Memory/LinearAllocatorTests.cc
@@ -1,3 +1,15 @@
 #include "LinearAllocatorTests.hpp"
+#include "../TestManager.hpp"
+#include "../Expect.hpp"
+
+#include <Memory/LinearAllocator.hpp>
+
+namespace flatearth {
+namespace tests {
+
+
+
+}
+}
 
 
--- tests/src/Memory/LinearAllocatorTests.hpp
+++ tests/src/Memory/LinearAllocatorTests.hpp
@@ -1,6 +1,12 @@
 #ifndef _FLATEARTH_TESTS_LINEAR_ALLOCATOR_HPP
 #define _FLATEARTH_TESTS_LINEAR_ALLOCATOR_HPP
 
+namespace flatearth {
+namespace tests {
+
 void LinearAllocatorRegisterTests();
+
+}
+} // namespace flatearth
 
 #endif // _FLATEARTH_TESTS_LINEAR_ALLOCATOR_HPP
