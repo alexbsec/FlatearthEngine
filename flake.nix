8158e982c8f8097a7dffa24c62cb6765543ec476
alexbsec <j.a.c.buschinelli@gmail.com>
2025-04-03 00:45:53 -0300

    changed nix

--- flake.nix
+++ flake.nix
@@ -35,17 +35,20 @@
 
           buildInputs = nativeBuildInputs;
 
-        shellHook = ''
-          echo "[Flatearth] Dev shell pronta 🚀"
-          echo "Use: build-engine | build-tests | run-tests"
+shellHook = ''
+  echo "[Flatearth] Dev shell pronta 🚀"
+  echo "Use: build-engine | build-tests | run-tests"
 
-          export PKG_CONFIG_PATH="${pkgs.xorg.libxcb.dev}/lib/pkgconfig:${pkgs.xorg.libX11.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-          export CMAKE_PREFIX_PATH="${pkgs.xorg.libxcb}:${pkgs.xorg.libX11}:${pkgs.xorg.libXdmcp}:${pkgs.xorg.libXau}:${pkgs.fmt}:${pkgs.vulkan-loader}:${pkgs.vulkan-headers}:${pkgs.vulkan-tools}:${pkgs.pkg-config}:${pkgs.gcc}:${pkgs.cmake}"
+  export PKG_CONFIG_PATH="${pkgs.xorg.libxcb.dev}/lib/pkgconfig:${pkgs.xorg.libX11.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+  export CMAKE_PREFIX_PATH="${pkgs.xorg.libxcb}:${pkgs.xorg.libX11}:${pkgs.xorg.libXdmcp}:${pkgs.xorg.libXau}:${pkgs.fmt}:${pkgs.vulkan-loader}:${pkgs.vulkan-headers}:${pkgs.vulkan-tools}:${pkgs.pkg-config}:${pkgs.gcc}:${pkgs.cmake}"
+  export LIBRARY_PATH="${pkgs.xorg.libXdmcp}/lib:${pkgs.xorg.libXau}/lib:$LIBRARY_PATH"
+  export NIX_LDFLAGS="-L${pkgs.xorg.libXdmcp}/lib -L${pkgs.xorg.libXau}/lib"
 
-          alias build-engine='cmake -S engine -B engine/build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake --build engine/build'
-          alias build-tests='cmake -S tests -B tests/build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake --build tests/build'
-          alias run-tests='./tests/build/flatearth_tests'
-        '';
+  alias build-engine='cmake -S engine -B engine/build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake --build engine/build'
+  alias build-tests='cmake -S tests -B tests/build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake --build tests/build'
+  alias run-tests='./tests/build/flatearth_tests'
+'';
+
 
         };
       });
